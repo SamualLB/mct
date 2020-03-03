185a8a2e2a4c65725e0766ad2396a3c817a3dd4a
Samual Black <samuallb@hotmail.com>
2020-03-03 12:41:00 +0000

    Simple CSVParser example

--- src/main/java/uk/ac/keele/csc20041/mct/Application.java
+++ src/main/java/uk/ac/keele/csc20041/mct/Application.java
@@ -1,16 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uk.ac.keele.csc20041.mct;
 
-/**
- *
- * @author sam
- */
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("Multiple Choice Test Application!");
+        
+        // Test CSV Parsing
+        Reader in = new FileReader("questions.csv");
+        int qs = 0;
+        for (CSVRecord record : CSVFormat.DEFAULT.withHeader("question", "a", "b", "c", "d", "answer").parse(in)) {
+            System.out.println("Q" + (qs+1) + ": " + record.get("question"));
+            System.out.println();
+            System.out.println("A: " + record.get("a"));
+            System.out.println("B: " + record.get("b"));
+            System.out.println("C: " + record.get("c"));
+            System.out.println("D: " + record.get("d"));
+            System.out.println();
+            System.out.println("Answer: " + record.get("answer"));
+            System.out.println("==========");
+            qs++;
+        }
     }
 }
