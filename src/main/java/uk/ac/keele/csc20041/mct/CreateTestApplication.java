bfa5bfdea6ba4cbd4f524c928651669ef8f9e98f
Samual Black <samuallb@hotmail.com>
2020-03-03 19:04:46 +0000

    Creating an test progress

--- src/main/java/uk/ac/keele/csc20041/mct/CreateTestApplication.java
+++ src/main/java/uk/ac/keele/csc20041/mct/CreateTestApplication.java
@@ -1,15 +1,33 @@
 package uk.ac.keele.csc20041.mct;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * Called through
  * ```
  * mct create 'exam_name' 'test_name'
  * ```
- * 
+ *
  * @author Samual
  */
 public class CreateTestApplication {
     public static void main(String[] args) {
-        System.out.println("Creating test... " + args.length);
+        if (args.length <= 0) {
+            printHelp();
+            return;
+        }
+        try {
+            Exam exam = new Exam(args[0]);
+            System.out.println("Creating test from exam...");
+        } catch (FileNotFoundException ex) {
+            System.err.println("Cannot find file \"" + args[0] + "\"");
+        } catch (IOException ex) {
+            System.err.println("System error. " + ex.getLocalizedMessage());
+        }
+    }
+    
+    public static void printHelp() {
+        System.out.println("Create help...");
     }
 }
